fc78f8370f4518e2d9e3361c02e7d4e626991bbb
Rohit7008 <rohitpottavathini07@gmail.com>
2025-04-17 19:52:27 +0530

    Remove tagline text from Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -22,7 +22,6 @@ const Logo = ({ className = "", variant = 'default', showTagline = true }: LogoP
         />
         {showTagline && (
           <div className={`ml-2 text-xs ${taglineColor} hidden md:block`}>
-            Your Gateway to MBBS Abroad
           </div>
         )}
       </div>
